2109eccf75680b7beda325874def24a63a399629
MAONEZZE <ruansanchezskt@gmail.com>
2023-06-29 16:12:50 -0300

    .

---
+++ TestesDonaMaria.Dominio/Compartilhado/EntidadeBase.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace TestesDonaMaria.Dominio.Compartilhado
+{
+    public abstract class EntidadeBase<T>
+    {
+        public int id { get; set; }
+
+        public abstract string[] Validar();
+
+        public abstract void AtualizarInformacoes(T entidade);
+
+        public abstract bool VerificarRepeticao(T entidade);
+
+        public abstract string ObterCampoSQL(bool ehParametro = false);
+
+        public abstract SqlParameter[] ObterParametroSQL();
+    }
+}
