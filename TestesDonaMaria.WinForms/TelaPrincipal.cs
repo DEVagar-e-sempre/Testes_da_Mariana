57665504fd1d59fe2b4475288d64cb365db49b4a
MAONEZZE <ruansanchezskt@gmail.com>
2023-06-29 14:41:12 -0300

    começo

---
+++ TestesDonaMaria.WinForms/TelaPrincipal.cs
@@ -0,0 +1,10 @@
+namespace TestesDonaMaria.WinForms
+{
+    public partial class TelaPrincipal : Form
+    {
+        public TelaPrincipal()
+        {
+            InitializeComponent();
+        }
+    }
+}
