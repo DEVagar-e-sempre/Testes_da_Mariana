3c6dc59d44fa5ffd72887368ea427f5ab517fda9
FranciscoPaixao <26686029+FranciscoPaixao@users.noreply.github.com>
2023-07-02 19:57:47 -0300

    Finalização do Repositório Base

--- TestesDonaMaria.Dominio/Compartilhado/EntidadeBase.cs
+++ TestesDonaMaria.Dominio/Compartilhado/EntidadeBase.cs
@@ -4,16 +4,30 @@ namespace TestesDonaMaria.Dominio.Compartilhado
 {
     public abstract class EntidadeBase<T>
     {
+
+        public EntidadeBase(SqlDataReader registro)
+        {
+            AtualizarInformacoes(registro);
+        }
+        public EntidadeBase()
+        {
+
+        }
+
         public int id { get; set; }
 
         public abstract string[] Validar();
 
         public abstract void AtualizarInformacoes(T entidade);
 
+        public abstract void AtualizarInformacoes(SqlDataReader registro);
+
         public abstract bool VerificarRepeticao(T registro);
 
         public abstract string ObterCampoSQL(bool ehParametro = false);
 
         public abstract SqlParameter[] ObterParametroSQL();
+
+        public abstract string ObterCampoUpdate();
     }
 }
