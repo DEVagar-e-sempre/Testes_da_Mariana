1636fc401ba7f78e491150d43f8c6f810340c5f8
Ruan Sanchez <103606152+MAONEZZE@users.noreply.github.com>
2023-07-02 21:34:36 -0300

    tela principal quase pronta

--- TestesDonaMaria.WinForms/TelaPrincipal.Designer.cs
+++ TestesDonaMaria.WinForms/TelaPrincipal.Designer.cs
@@ -28,18 +28,288 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TelaPrincipal));
+            painel_botoesEntidades = new Panel();
+            label2 = new Label();
+            button4 = new Button();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            painel_principal = new Panel();
+            toolStrip1 = new ToolStrip();
+            toolStripButton1 = new ToolStripButton();
+            toolStripButton2 = new ToolStripButton();
+            toolStripButton3 = new ToolStripButton();
+            toolStripSeparator1 = new ToolStripSeparator();
+            toolStripButton4 = new ToolStripButton();
+            toolStripButton5 = new ToolStripButton();
+            toolStripSeparator2 = new ToolStripSeparator();
+            toolStripButton6 = new ToolStripButton();
+            statusStrip1 = new StatusStrip();
+            toolStripStatusLabel1 = new ToolStripStatusLabel();
+            panel1 = new Panel();
+            label1 = new Label();
+            painel_botoesEntidades.SuspendLayout();
+            painel_principal.SuspendLayout();
+            toolStrip1.SuspendLayout();
+            statusStrip1.SuspendLayout();
+            panel1.SuspendLayout();
             SuspendLayout();
             // 
-            // Form1
+            // painel_botoesEntidades
+            // 
+            painel_botoesEntidades.BackColor = Color.CornflowerBlue;
+            painel_botoesEntidades.Controls.Add(label2);
+            painel_botoesEntidades.Controls.Add(button4);
+            painel_botoesEntidades.Controls.Add(button3);
+            painel_botoesEntidades.Controls.Add(button2);
+            painel_botoesEntidades.Controls.Add(button1);
+            painel_botoesEntidades.Dock = DockStyle.Left;
+            painel_botoesEntidades.Location = new Point(0, 0);
+            painel_botoesEntidades.Name = "painel_botoesEntidades";
+            painel_botoesEntidades.Size = new Size(192, 457);
+            painel_botoesEntidades.TabIndex = 0;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe Print", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(0, 434);
+            label2.Name = "label2";
+            label2.Size = new Size(132, 23);
+            label2.TabIndex = 1;
+            label2.Text = "DEVagar e sempre";
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.CornflowerBlue;
+            button4.FlatAppearance.BorderColor = Color.CornflowerBlue;
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.Font = new Font("Segoe Print", 15.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            button4.ForeColor = Color.Black;
+            button4.Image = Properties.Resources.task_FILL0_wght400_GRAD200_opsz24;
+            button4.ImageAlign = ContentAlignment.MiddleLeft;
+            button4.Location = new Point(3, 240);
+            button4.Name = "button4";
+            button4.Size = new Size(186, 47);
+            button4.TabIndex = 4;
+            button4.Text = "Teste";
+            button4.TextImageRelation = TextImageRelation.ImageBeforeText;
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.CornflowerBlue;
+            button3.FlatAppearance.BorderColor = Color.CornflowerBlue;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("Segoe Print", 15.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            button3.ForeColor = Color.Black;
+            button3.Image = Properties.Resources.menu_book_FILL0_wght400_GRAD200_opsz24;
+            button3.ImageAlign = ContentAlignment.MiddleLeft;
+            button3.Location = new Point(3, 134);
+            button3.Name = "button3";
+            button3.Size = new Size(186, 47);
+            button3.TabIndex = 3;
+            button3.Text = "Matéria";
+            button3.TextImageRelation = TextImageRelation.ImageBeforeText;
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.CornflowerBlue;
+            button2.FlatAppearance.BorderColor = Color.CornflowerBlue;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Segoe Print", 15.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            button2.ForeColor = Color.Black;
+            button2.Image = Properties.Resources.format_list_numbered_FILL0_wght400_GRAD200_opsz24;
+            button2.ImageAlign = ContentAlignment.MiddleLeft;
+            button2.Location = new Point(3, 187);
+            button2.Name = "button2";
+            button2.Size = new Size(186, 47);
+            button2.TabIndex = 2;
+            button2.Text = "Questões";
+            button2.TextImageRelation = TextImageRelation.ImageBeforeText;
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.CornflowerBlue;
+            button1.FlatAppearance.BorderColor = Color.CornflowerBlue;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe Print", 15.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            button1.ForeColor = Color.Black;
+            button1.Image = Properties.Resources.collections_bookmark_FILL0_wght400_GRAD200_opsz24;
+            button1.ImageAlign = ContentAlignment.MiddleLeft;
+            button1.Location = new Point(3, 81);
+            button1.Name = "button1";
+            button1.Size = new Size(186, 47);
+            button1.TabIndex = 1;
+            button1.Text = "Disciplina";
+            button1.TextImageRelation = TextImageRelation.ImageBeforeText;
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // painel_principal
+            // 
+            painel_principal.Controls.Add(toolStrip1);
+            painel_principal.Controls.Add(statusStrip1);
+            painel_principal.Dock = DockStyle.Bottom;
+            painel_principal.Location = new Point(192, 73);
+            painel_principal.Name = "painel_principal";
+            painel_principal.Size = new Size(714, 384);
+            painel_principal.TabIndex = 1;
+            // 
+            // toolStrip1
+            // 
+            toolStrip1.Items.AddRange(new ToolStripItem[] { toolStripButton1, toolStripButton2, toolStripButton3, toolStripSeparator1, toolStripButton4, toolStripButton5, toolStripSeparator2, toolStripButton6 });
+            toolStrip1.Location = new Point(0, 0);
+            toolStrip1.Name = "toolStrip1";
+            toolStrip1.Size = new Size(714, 25);
+            toolStrip1.TabIndex = 1;
+            toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            toolStripButton1.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            toolStripButton1.Image = (Image)resources.GetObject("toolStripButton1.Image");
+            toolStripButton1.ImageTransparentColor = Color.Magenta;
+            toolStripButton1.Name = "toolStripButton1";
+            toolStripButton1.Size = new Size(23, 22);
+            toolStripButton1.Text = "toolStripButton1";
+            // 
+            // toolStripButton2
+            // 
+            toolStripButton2.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            toolStripButton2.Image = (Image)resources.GetObject("toolStripButton2.Image");
+            toolStripButton2.ImageTransparentColor = Color.Magenta;
+            toolStripButton2.Name = "toolStripButton2";
+            toolStripButton2.Size = new Size(23, 22);
+            toolStripButton2.Text = "toolStripButton2";
+            // 
+            // toolStripButton3
+            // 
+            toolStripButton3.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            toolStripButton3.Image = (Image)resources.GetObject("toolStripButton3.Image");
+            toolStripButton3.ImageTransparentColor = Color.Magenta;
+            toolStripButton3.Name = "toolStripButton3";
+            toolStripButton3.Size = new Size(23, 22);
+            toolStripButton3.Text = "toolStripButton3";
+            // 
+            // toolStripSeparator1
+            // 
+            toolStripSeparator1.Name = "toolStripSeparator1";
+            toolStripSeparator1.Size = new Size(6, 25);
+            // 
+            // toolStripButton4
+            // 
+            toolStripButton4.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            toolStripButton4.Image = (Image)resources.GetObject("toolStripButton4.Image");
+            toolStripButton4.ImageTransparentColor = Color.Magenta;
+            toolStripButton4.Name = "toolStripButton4";
+            toolStripButton4.Size = new Size(23, 22);
+            toolStripButton4.Text = "toolStripButton4";
+            // 
+            // toolStripButton5
+            // 
+            toolStripButton5.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            toolStripButton5.Image = (Image)resources.GetObject("toolStripButton5.Image");
+            toolStripButton5.ImageTransparentColor = Color.Magenta;
+            toolStripButton5.Name = "toolStripButton5";
+            toolStripButton5.Size = new Size(23, 22);
+            toolStripButton5.Text = "toolStripButton5";
+            // 
+            // toolStripSeparator2
+            // 
+            toolStripSeparator2.Name = "toolStripSeparator2";
+            toolStripSeparator2.Size = new Size(6, 25);
+            // 
+            // toolStripButton6
+            // 
+            toolStripButton6.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            toolStripButton6.Image = (Image)resources.GetObject("toolStripButton6.Image");
+            toolStripButton6.ImageTransparentColor = Color.Magenta;
+            toolStripButton6.Name = "toolStripButton6";
+            toolStripButton6.Size = new Size(23, 22);
+            toolStripButton6.Text = "toolStripButton6";
+            // 
+            // statusStrip1
+            // 
+            statusStrip1.Items.AddRange(new ToolStripItem[] { toolStripStatusLabel1 });
+            statusStrip1.Location = new Point(0, 362);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Size = new Size(714, 22);
+            statusStrip1.TabIndex = 0;
+            statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            toolStripStatusLabel1.Size = new Size(118, 17);
+            toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.RoyalBlue;
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(906, 75);
+            panel1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe Print", 18F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            label1.ForeColor = Color.Black;
+            label1.Location = new Point(398, 18);
+            label1.Name = "label1";
+            label1.Size = new Size(313, 43);
+            label1.TabIndex = 0;
+            label1.Text = "Testes da Dona Mariana";
+            // 
+            // TelaPrincipal
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(634, 430);
-            Name = "Form1";
-            Text = "Form1";
+            ClientSize = new Size(906, 457);
+            Controls.Add(panel1);
+            Controls.Add(painel_principal);
+            Controls.Add(painel_botoesEntidades);
+            Name = "TelaPrincipal";
+            Text = "Tela Principal";
+            painel_botoesEntidades.ResumeLayout(false);
+            painel_botoesEntidades.PerformLayout();
+            painel_principal.ResumeLayout(false);
+            painel_principal.PerformLayout();
+            toolStrip1.ResumeLayout(false);
+            toolStrip1.PerformLayout();
+            statusStrip1.ResumeLayout(false);
+            statusStrip1.PerformLayout();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel painel_botoesEntidades;
+        private Panel painel_principal;
+        private Button button1;
+        private StatusStrip statusStrip1;
+        private Button button4;
+        private Button button3;
+        private Button button2;
+        private ToolStripStatusLabel toolStripStatusLabel1;
+        private Panel panel1;
+        private Label label2;
+        private Label label1;
+        private ToolStrip toolStrip1;
+        private ToolStripButton toolStripButton1;
+        private ToolStripButton toolStripButton2;
+        private ToolStripButton toolStripButton3;
+        private ToolStripSeparator toolStripSeparator1;
+        private ToolStripButton toolStripButton4;
+        private ToolStripButton toolStripButton5;
+        private ToolStripSeparator toolStripSeparator2;
+        private ToolStripButton toolStripButton6;
     }
 }
