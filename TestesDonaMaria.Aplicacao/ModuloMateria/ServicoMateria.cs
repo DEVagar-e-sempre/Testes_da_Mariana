cf59d62a5c27bb0c1446d9d2451c45d26aa37411
Ruan Sanchez <ruansanchezskt@gmail.com>
2023-07-18 19:47:12 -0300

    camada serviço - materia e questao

--- TestesDonaMaria.Aplicacao/ModuloMateria/ServicoMateria.cs
+++ TestesDonaMaria.Aplicacao/ModuloMateria/ServicoMateria.cs
@@ -5,5 +5,10 @@ namespace TestesDonaMaria.Aplicacao.ModuloMateria
 {
     public class ServicoMateria : ServicoBase <Materia, RepositorioSQLMateria>
     {
+        protected override string MsgErro => "Esta Materia está relacionada a uma Questão e não pode ser excluída";
+        public ServicoMateria() : base() 
+        {
+            
+        }
     }
 }
--- TestesDonaMaria.Aplicacao/ModuloQuestao/ServicoQuestao.cs
+++ TestesDonaMaria.Aplicacao/ModuloQuestao/ServicoQuestao.cs
@@ -5,5 +5,10 @@ namespace TestesDonaMaria.Aplicacao.ModuloQuestao
 {
     public class ServicoQuestao : ServicoBase<Questao, RepositorioSQLQuestao>
     {
+        protected override string MsgErro => "Esta Questão está relacionada a um Teste e não pode ser excluída";
+        public ServicoQuestao() : base()
+        {
+            
+        }
     }
 }
